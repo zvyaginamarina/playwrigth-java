b823cd5777dbe8ffedf8f31e433f71312e4e6cec
mzviagina <mzviagina@custis.ru>
2026-06-01 20:06:47 +0400

    created project and first test

---
+++ src/test/java/auto/tests/PlaywrightExampleTest.java
@@ -0,0 +1,40 @@
+package auto.tests;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserContext;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+public class PlaywrightExampleTest {
+
+    Playwright playwright;
+    Browser browser;
+    BrowserContext context;
+    Page page;
+
+    @BeforeEach
+    void setUp() {
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        context = browser.newContext();
+        page = context.newPage();
+    }
+
+    @Test
+    void testPageTitle() {
+        page.navigate("https://www.google.com");
+        String title = page.title();
+        Assertions.assertEquals("Google", title);
+    }
+
+    @AfterEach
+    void tearDown() {
+        playwright.close();
+    }
+}
